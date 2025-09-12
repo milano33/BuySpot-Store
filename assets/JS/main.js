cf4b8c13c408a74faa6a7908a774e3b1297f2af8
omar sakr <DevOmarHussien@gmail.com>
2025-09-12 19:05:44 +0300

    Save all changes

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -388,7 +388,7 @@ document.addEventListener('DOMContentLoaded', () => {
             logoutBtn.addEventListener("click", (e) => {
                 e.preventDefault();
                 // console.log(' Logout button clicked');
-                const redirectUrl = window.location.pathname.includes('pages/') ? "../BuySpot-Store/index.html" : "../../index.html";
+                const redirectUrl = window.location.pathname.includes('pages/') ? "../index.html" : "../../index.html";
                 window.userDataManager.handleUserLogout(redirectUrl);
             });
         }
